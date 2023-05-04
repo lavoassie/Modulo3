839562c1b019663d775c385971289882cc3bf540
Genesis Voisier <voassie@gmail.com>
2023-05-04 18:05:57 -0400

    Se agrega el valor a gastos

--- APP de presupuesto/asserts/js/index.js
+++ APP de presupuesto/asserts/js/index.js
@@ -1,7 +1,26 @@
-let inputPresupuesto = document.getElementById("presupuesto")
-inputPresupuesto.addEventListener('keyup',function(){
-  let presupuesto = inputPresupuesto.value.replace(/\D/g,"").replace(/\B(?=(\d{3})+(?!\d))/g,".")
-  inputPresupuesto.value = presupuesto
-  console.log(presupuesto)
-  document.getElementById("totalPresupuesto").innerHTML = `CLP: ${presupuesto}`
+//Añadiendo el presupuesto del usuario
+let botonPresupuesto = document.getElementById("botonPresupuesto");
+botonPresupuesto.addEventListener('click',function(){
+  let inputPresupuesto = document.getElementById("inputPresupuesto")
+  let inputValue = inputPresupuesto.value.replace(/\D/g,"").replace(/\B(?=(\d{3})+(?!\d))/g,".")
+  console.log(inputValue)
+  document.getElementById("totalPresupuesto").innerHTML = `$ ${inputValue}`
 })
+
+//añadiendo gastos
+
+let botonAgregar = document.getElementById("buttonAdd");
+botonAgregar.addEventListener('click',function(){
+  let inputNombre = document.getElementById("inputName");
+  let inputValor = document.getElementById("inputValor");
+  let inputValueName = inputNombre.value
+  let inputValueCLP = inputValor.value
+  console.log(inputValueName, inputValueCLP)
+  document.getElementById("nombreGasto").innerHTML = `${inputValueName}`
+  document.getElementById("valorGasto").innerHTML = `$ ${inputValueCLP}`
+  document.getElementById("gasto").innerHTML = `$ ${inputValueCLP}`
+})
+
+
+
+//restar 
