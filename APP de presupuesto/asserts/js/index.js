cf426e23c2fe8e7389094e7a93cac19861f36a31
Genesis Voisier <voassie@gmail.com>
2023-05-05 11:32:09 -0400

    Se añade boostrap para el diseño

--- APP de presupuesto/asserts/js/index.js
+++ APP de presupuesto/asserts/js/index.js
@@ -1,26 +1,25 @@
-//Añadiendo el presupuesto del usuario
-let botonPresupuesto = document.getElementById("botonPresupuesto");
-botonPresupuesto.addEventListener('click',function(){
-  let inputPresupuesto = document.getElementById("inputPresupuesto")
-  let inputValue = inputPresupuesto.value.replace(/\D/g,"").replace(/\B(?=(\d{3})+(?!\d))/g,".")
-  console.log(inputValue)
-  document.getElementById("totalPresupuesto").innerHTML = `$ ${inputValue}`
+//Creando las variables para capturar los input y botones
+let inputPresupuesto = document.getElementById("inputPresupuesto");
+let botonCalcular = document.getElementById("btnCalcular");
+let inputNombreGasto = document.getElementById("inputNombreGasto");
+let inputMontoGasto = document.getElementById("inputMontoGasto");
+let presupuesto = document.getElementById("presupuesto"); //es el id del párrafo que se modificará en el HTML
+
+
+//Capturando el presupuesto del usuario y mostrándolo en la página
+botonCalcular.addEventListener("click", function () {
+    presupuesto.innerHTML = inputPresupuesto.value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".")
 })
 
 //añadiendo gastos
+let botonAgregar = document.getElementById("btnAgregarGasto");
+let gastos = document.getElementById("gastos");
+let saldo = document.getElementById("saldo")
 
-let botonAgregar = document.getElementById("buttonAdd");
-botonAgregar.addEventListener('click',function(){
-  let inputNombre = document.getElementById("inputName");
-  let inputValor = document.getElementById("inputValor");
-  let inputValueName = inputNombre.value
-  let inputValueCLP = inputValor.value
-  console.log(inputValueName, inputValueCLP)
-  document.getElementById("nombreGasto").innerHTML = `${inputValueName}`
-  document.getElementById("valorGasto").innerHTML = `$ ${inputValueCLP}`
-  document.getElementById("gasto").innerHTML = `$ ${inputValueCLP}`
+botonAgregar.addEventListener("click", function () {
+    gastos.innerHTML = inputNombreGasto.value;
+    saldo.innerHTML = inputMontoGasto.value;
+    // document.getElementById("nombreGasto").innerHTML = `${inputValueName}`
+    // document.getElementById("valorGasto").innerHTML = `$ ${inputValueCLP}`
+    // document.getElementById("gasto").innerHTML = `$ ${inputValueCLP}`
 })
-
-
-
-//restar 
